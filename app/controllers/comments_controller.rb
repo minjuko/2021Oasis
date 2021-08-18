f1283db4d1d1b1e34e2e79442631b94abbd201de
hy01n <hobbyhyoin@naver.com>
2021-08-18 21:03:54 +0900

    list almost done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    @ride   = current_user.rides.find(params[:comment][:ride_id])
-    @comment = @ride.comments.build(params[:comment].permit(:comment))
-    @comment.save!
+    @ride = Ride.find(params[:ride_id])
+    @comment = @ride.comments.create(params[:comment])#.permit(:departure, :arrival)
+    @comment.user = current_user #이 comments의 user_id를 저장하여, 댓글을 단 사용자의 이메일을 노출할 수 있음!
+    @comment.save
     redirect_to request.referrer
   end
 
