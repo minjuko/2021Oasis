b344c3ab8350ea711625d0a17ea59c832cceff55
hy01n <hobbyhyoin@naver.com>
2021-08-18 22:14:56 +0900

    fix all error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   def create
     @ride = Ride.find(params[:ride_id])
-    @comment = @ride.comments.create(params[:comment])#.permit(:departure, :arrival)
-    @comment.user = current_user #이 comments의 user_id를 저장하여, 댓글을 단 사용자의 이메일을 노출할 수 있음!
+    @comment = @ride.comments.create(params[:comment])#.permit(:ride_id)
+    @comment.user_id = current_user.id #이 comments의 user_id를 저장하여, 댓글을 단 사용자의 이메일을 노출할 수 있음!
     @comment.save
     redirect_to request.referrer
   end
