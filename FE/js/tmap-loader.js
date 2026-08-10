3c73854595a220ee221c309486054c6df7500e65
minjuko <rhalswn0526@gmail.com>
2026-08-10 22:04:53 +0900

    chore: archive 공개 안전성 정리

--- FE/js/tmap-loader.js
+++ FE/js/tmap-loader.js
@@ -2,6 +2,7 @@
   var appKey = window.TMAP_APP_KEY;
 
   if (!appKey || appKey === "YOUR_TMAP_APP_KEY") {
+    window.TMAP_SDK_STATUS = "missing-key";
     console.error(
       "TMAP App Key가 없습니다. tmap-config.example.js를 " +
         "tmap-config.local.js로 복사한 뒤 키를 입력해주세요."
@@ -12,6 +13,6 @@
   document.write(
     '<script src="https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=' +
       encodeURIComponent(appKey) +
-      '"><\/script>'
+      '" onerror="window.TMAP_SDK_STATUS=\'load-failed\'"><\/script>'
   );
 })();
